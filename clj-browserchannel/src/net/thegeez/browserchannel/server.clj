9cc8bf5234cd95468837f0ffeb8b4fa7f93fc5a4
gered <gered@blarg.ca>
2016-05-08 20:30:25 -0400

    formatting

--- clj-browserchannel/src/net/thegeez/browserchannel/server.clj
+++ clj-browserchannel/src/net/thegeez/browserchannel/server.clj
@@ -766,10 +766,10 @@
 
 ;; straight from google
 (def standard-headers
-  {"Content-Type" "text/plain"
-   "Cache-Control" "no-cache, no-store, max-age=0, must-revalidate"
-   "Pragma" "no-cache"
-   "Expires" "Fri, 01 Jan 1990 00:00:00 GMT"
+  {"Content-Type"           "text/plain"
+   "Cache-Control"          "no-cache, no-store, max-age=0, must-revalidate"
+   "Pragma"                 "no-cache"
+   "Expires"                "Fri, 01 Jan 1990 00:00:00 GMT"
    "X-Content-Type-Options" "nosniff"})
 
 (def default-options
