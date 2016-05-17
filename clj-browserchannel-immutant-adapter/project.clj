f48818b137f59d596af44edcb103dbac45fc382d
gered <gered@blarg.ca>
2016-05-17 14:37:25 -0400

    move clj-browserchannel dependency to provided
    since it will always be included as a dependency of projects using
    either of these adapters anyway

--- clj-browserchannel-immutant-adapter/project.clj
+++ clj-browserchannel-immutant-adapter/project.clj
@@ -2,9 +2,10 @@
   :description  "Immutant async adapter for BrowserChannel"
   :url          "https://github.com/gered/clj-browserchannel/tree/master/clj-browserchannel-immutant-adapter"
 
-  :dependencies [[gered/clj-browserchannel "0.3"]]
+  :dependencies []
   
   :profiles     {:provided
                  {:dependencies
                   [[org.clojure/clojure "1.8.0"]
-                   [org.immutant/web "2.1.4"]]}})
+                   [org.immutant/web "2.1.4"]
+                   [gered/clj-browserchannel "0.3"]]}})
--- clj-browserchannel-jetty-adapter/project.clj
+++ clj-browserchannel-jetty-adapter/project.clj
@@ -2,11 +2,12 @@
   :description  "Jetty async adapter for BrowserChannel"
   :url          "https://github.com/gered/clj-browserchannel/tree/master/clj-browserchannel-jetty-adapter"
 
-  :dependencies [[gered/clj-browserchannel "0.3"]]
+  :dependencies []
 
   :profiles     {:provided
                   {:dependencies
                     [[org.clojure/clojure "1.8.0"]
                      [ring/ring-core "1.4.0"]
                      [ring/ring-servlet "1.4.0"]
-                     [ring/ring-jetty-adapter "1.4.0"]]}})
+                     [ring/ring-jetty-adapter "1.4.0"]
+                     [gered/clj-browserchannel "0.3"]]}})
